59f8075caf79f5c2668016b69ba853e5419ab894
github username <github email address>
2018-05-10 10:55:44 -0400

    Created form and dropdown select option

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ const App = (function() {
   return class App {
     static init() {
       console.log("I'm in app");
+      App.populateOptions()
+    }
+
+    static populateOptions() {
+      let opts = ["Accounting", "Dancing", "Marketing", "Product Management", "Shopping"]
+      let dropdown = document.getElementById('dropdownOptions')
+      for(let i = 0; i < opts.length; i++) {
+        let option = document.createElement("option")
+        option.innerText = opts[i]
+        option.setAttribute("id", i)
+        dropdown.append(option)
+      }
+      console.log(opts);
     }
   }
 })();
